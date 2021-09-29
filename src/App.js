2a79ff306722b0f1ffe508c469b516651dda3874
Tony <tmitchell@greenmountaintechnology.com>
2021-09-29 12:23:30 -0500

    Section 3.48 also added Prettier and ESLint to application

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import {Component} from "react";
-import './App.css';
+import {Component} from 'react'
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
+
+import './App.css'
 
 class App extends Component {
     constructor(props) {
-        super(props);
+        super(props)
 
         this.state = {
-            monsters: []
-        };
+            monsters: [],
+            searchField: ''
+        }
     }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({monsters: users}));
+            .then(users => this.setState({monsters: users}))
     }
 
+    handleChange = e => this.setState({searchField: e.target.value})
+
     render() {
+        const {monsters, searchField} = this.state
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
         return (
             <div className="App">
-                {this.state.monsters.map(monster => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+                <SearchBox
+                    placeholder='search monsters'
+                    handleChange={this.handleChange}/>
+                <CardList monsters={filteredMonsters}/>
             </div>
         )
     }
 }
 
-export default App;
+export default App
